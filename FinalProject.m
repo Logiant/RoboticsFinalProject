1a90fb5302fdef99491a3e0dfb9ebc70908de703
Logiant <beaverl@msoe.edu>
2015-05-03 22:22:00 -0500

    Created initial matlab project header

---
+++ FinalProject.m
@@ -0,0 +1,8 @@
+% ----------------------------------------------------------------------
+% Main File   : FinalProject.m
+% Source Files: None
+% Description : Final Project Template for ME498 - Robotics
+% Author: Logan Beaver, Justin Collins
+% Date: 5/3/2015
+% Bugs: none
+% -------------------------------------------------------------------
