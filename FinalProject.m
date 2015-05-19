fb0e0cd56a0283693ef25396ab5dd74011a0dc4e
Logiant <beaverl@msoe.edu>
2015-05-18 22:04:49 -0500

    Life is meaningless and we're all going to die

--- FinalProject.m
+++ FinalProject.m
@@ -18,6 +18,8 @@ Bm = 1;
 Jm = 1; J1 = 1;
 m1 = 1; g1 = 1; L1 = 1; B1 = 1;
 Jw=1; mc=1; m2=1; m3=1; dc=1;
+%stabbing offset
+offset = 0.02; %m
 
 %calculate a 2xN matrix of mellon (X, Y) positions
 mellonsPos = (rand(2,numMellons) - 0.5) * 2; %between 0 and 1
@@ -38,9 +40,9 @@ for q = 1:numMellons
     mellonsPos(:, nearestIndex) = []; %remove nearest mellon from storage
     numMellons = numMellons - 1; %decrement mellon array size
     %generate the trajectory to the target mellon
-    dPosition = distance(position(1), position(2), targetPos(1), targetPos(2));
+    dPosition = distance(position(1), position(2), targetPos(1), targetPos(2)) - offset;
     dTheta = atan2(targetPos(1) - position(1), targetPos(2) - position(2));
-    dt = dPosition / speed; %calculate time to keep the average speed
+    dt = abs(dPosition / speed); %calculate time to keep the average speed
     
     %convert dTheta to a wheel average difference
     wheelDiff = dTheta*wheelSpacing/wheelRadius; %phiL - phiR
@@ -64,23 +66,32 @@ for q = 1:numMellons
     
     time = time + dt;
     
-    tArray(q) = time;
+    tArray(q) = time;   
 end
 
 %run simulation for left wheel
-coeffs = leftCoeffs;
-sim('wheelControl', [0 time]);
+wCoeff = leftCoeffs;
+sim('wheel1', [0 time]);
 %get leftTheta vector from simulink
 leftTime = thetaActual.Time;
 leftAngle = thetaActual.Data;
 
 %run simulation for right wheel
-coeffs = rightCoeffs;
-sim('wheelControl', [0 time]);
+wCoeff = rightCoeffs;
+sim('wheel1', [0 time]);
 %get rightTheta vector from simulink
 rightTime = thetaActual.Time;
 rightAngle = thetaActual.Data;
 
 figure(2);
 plot(leftTime, leftAngle, '-k', rightTime, rightAngle, '-b');
+hold on;
+
+tArray = [0 tArray];
+for k = 1:length(tArray) - 1
+    plot(tArray(k:k+1), polyval(leftCoeffs(k,:), tArray(k:k+1)),'or');
+    plot(tArray(k:k+1), polyval(rightCoeffs(k,:), tArray(k:k+1)), 'ob');
+end
+
+
 
